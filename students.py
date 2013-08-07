167ec70f9ef5e25ddad9c2a1f43e511af9754920
swellington16 <swellington232@gmail.com>
2013-08-07 11:30:10 -0500

    Update students.py
    Altered the function calls to suit the changes to the simple_rdbms module.

--- students.py
+++ students.py
@@ -3,25 +3,26 @@ from simple_rdbms import *
 db = Database() #The database
 
 #students table
-db.createTable("students",["id","f_name","l_name"])
-db.get("students").insert((620024789,"Sean","Wellington"))
-db.get("students").insert((620045678,"Crystal","Francis"))
-db.get("students").insert((620089945,"Denar","Palmer"))
-db.get("students").insert((620034569,"Brian","Long"))
+db.createTable("students",["sid","f_name","l_name"])
+db.createTable("lecturers",["lid","f_name","l_name"])
+db.getTable("students").insert((620024789,"Sean","Wellington"))
+db.getTable("students").insert((620045678,"Crystal","Francis"))
+db.getTable("students").insert((620089945,"Denar","Palmer"))
+db.getTable("students").insert((620034569,"Brian","Long"))
 
 #Shortening the attributes
-sid = db.get("students").attr["id"]
-f_name = db.get("students").attr["f_name"]
-l_name = db.get("students").attr["l_name"]
+sid = db.getAttr("students","sid")
+f_name = db.getAttr("students","f_name")
+l_name = db.getAttr("students","l_name")
 
 #The query conditions
 q1 = lambda x: x[sid] == 620024789
 q2 = lambda x: x[f_name] > "C"
-q3 = lambda x: x[l_name][0] == "W"
+q3 = lambda x: getFirstCharacter(x[l_name]) == "W"
 
-#The execution of the queries
-t1 = db.get("students").select(q1,["id","l_name"])
-t2 = db.get("students").select(q2,["id"])
-t3 = db.get("students").select(q3,[])  
+#The execution of the queries and their result tables
+t1 = db.select_query("students",q1,["sid","l_name"])
+t2 = db.select_query("students",q2,["sid"])
+t3 = db.select_query("students",q3,[])  
 
 
