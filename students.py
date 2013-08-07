4a4fde4238923facf7203345a06d6a27dc9c0e0b
swellington16 <swellington232@gmail.com>
2013-08-07 12:17:01 -0500

    Update students.py
    Switched from insert() method to insert_query() method.

--- students.py
+++ students.py
@@ -5,10 +5,10 @@ db = Database() #The database
 #students table
 db.createTable("students",["sid","f_name","l_name"])
 db.createTable("lecturers",["lid","f_name","l_name"])
-db.getTable("students").insert((620024789,"Sean","Wellington"))
-db.getTable("students").insert((620045678,"Crystal","Francis"))
-db.getTable("students").insert((620089945,"Denar","Palmer"))
-db.getTable("students").insert((620034569,"Brian","Long"))
+db.insert_query("students",(620024789,"Sean","Wellington"))
+db.insert_query("students",(620045678,"Crystal","Francis"))
+db.insert_query("students",(620089945,"Denar","Palmer"))
+db.insert_query("students",(620034569,"Brian","Long"))
 
 #Shortening the attributes
 sid = db.getAttr("students","sid")
